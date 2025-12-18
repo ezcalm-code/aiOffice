0c83b2f3a4fe1fbe9943d0b784442793147b4612
wsj <calm_down2025@163.com>
2025-12-18 15:49:47 +0800

    front WebSocket 客户端实现

--- aioffice-front/src/services/index.ts
+++ aioffice-front/src/services/index.ts
@@ -5,6 +5,8 @@
 
 export * from './http';
 export * from './auth';
+export * from './websocket';
 
 export { default as http } from './http';
 export { default as auth } from './auth';
+export { default as websocket } from './websocket';
